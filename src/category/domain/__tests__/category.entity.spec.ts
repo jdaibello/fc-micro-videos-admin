441a837fb85d411239ded4f5b6cf710c7c3cf875
João Pedro <jpedrodaibello1999@outlook.com>
2024-05-17 14:48:17 +0000

    feat: add UUID to Category

--- src/category/domain/__tests__/category.entity.spec.ts
+++ src/category/domain/__tests__/category.entity.spec.ts
@@ -1,3 +1,4 @@
+import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
 import { Category } from "../category.entity";
 
 describe('Category Unit Tests', () => {
@@ -7,7 +8,7 @@ describe('Category Unit Tests', () => {
               name: "Movie",
             });
 
-            expect(category.category_id).toBeUndefined();
+            expect(category.category_id).toBeInstanceOf(Uuid);
             expect(category.name).toBe("Movie");
             expect(category.description).toBeNull();
             expect(category.is_active).toBeTruthy();
@@ -24,7 +25,7 @@ describe('Category Unit Tests', () => {
               created_at,
             });
 
-            expect(category.category_id).toBeUndefined();
+            expect(category.category_id).toBeInstanceOf(Uuid);
             expect(category.name).toBe("Movie");
             expect(category.description).toBe("Movie description");
             expect(category.is_active).toBeFalsy();
@@ -37,7 +38,7 @@ describe('Category Unit Tests', () => {
               description: "Movie description",
             });
 
-            expect(category.category_id).toBeUndefined();
+            expect(category.category_id).toBeInstanceOf(Uuid);
             expect(category.name).toBe("Movie");
             expect(category.description).toBe("Movie description");
             expect(category.is_active).toBeTruthy();
@@ -51,7 +52,7 @@ describe('Category Unit Tests', () => {
           name: "Movie",
         });
 
-        expect(category.category_id).toBeUndefined();
+        expect(category.category_id).toBeInstanceOf(Uuid);
         expect(category.name).toBe("Movie");
         expect(category.description).toBeNull();
         expect(category.is_active).toBeTruthy();
@@ -64,7 +65,7 @@ describe('Category Unit Tests', () => {
           description: "Movie description",
         });
 
-        expect(category.category_id).toBeUndefined();
+        expect(category.category_id).toBeInstanceOf(Uuid);
         expect(category.name).toBe("Movie");
         expect(category.description).toBe("Movie description");
         expect(category.is_active).toBeTruthy();
@@ -78,11 +79,28 @@ describe('Category Unit Tests', () => {
           is_active: false,
         });
 
-        expect(category.category_id).toBeUndefined();
+        expect(category.category_id).toBeInstanceOf(Uuid);
         expect(category.name).toBe("Movie");
         expect(category.description).toBe("Movie description");
         expect(category.is_active).toBeFalsy();
         expect(category.created_at).toBeInstanceOf(Date);
+      });
+    });
+
+    describe('category_id field', () => {
+      const arrange = [
+        { category_id: null }, { category_id: undefined }, { category_id: new Uuid() }
+      ];
+
+      test.each(arrange)('id = %j', ({category_id}) => {
+        const category = new Category({
+          category_id: category_id as any,
+          name: 'Movie',
+        });
+
+        if (category_id instanceof Uuid) {
+          expect(category.category_id).toBe(category_id);
+        }
       });
     });
 
