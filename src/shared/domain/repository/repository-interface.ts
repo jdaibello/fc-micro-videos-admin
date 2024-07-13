d9294c4c913efd45f035ebef16abb509b296bdf2
João Pedro <jpedrodaibello1999@outlook.com>
2024-07-13 11:20:42 -0300

    feat: create repository interface

---
+++ src/shared/domain/repository/repository-interface.ts
@@ -0,0 +1,14 @@
+import { Entity } from '../entity';
+import { ValueObject } from '../value-object';
+
+export interface IRepository<E extends Entity, EntityID extends ValueObject> {
+  insert(entity: E): Promise<void>;
+  bulkInsert(entities: E[]): Promise<void>;
+  update(entity: E): Promise<void>;
+  delete(entity_id: EntityID): Promise<void>;
+
+  findById(entity_id: EntityID): Promise<E>;
+  findAll(): Promise<E[]>;
+
+  getEntity(): new (...args: any[]) => E;
+}
