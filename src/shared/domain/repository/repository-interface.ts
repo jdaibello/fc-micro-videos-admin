bb45325e0899c44fe273329f5e4c3335dbe6c909
João Pedro <jpedrodaibello1999@outlook.com>
2024-07-17 09:25:46 -0300

    feat: creating repository interface for custom searches

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -1,5 +1,7 @@
 import { Entity } from '../entity';
 import { ValueObject } from '../value-object';
+import { SearchParams } from './search-params';
+import { SearchResult } from './search-result';
 
 export interface IRepository<E extends Entity, EntityID extends ValueObject> {
   insert(entity: E): Promise<void>;
@@ -11,4 +13,15 @@ export interface IRepository<E extends Entity, EntityID extends ValueObject> {
   findAll(): Promise<E[]>;
 
   getEntity(): new (...args: any[]) => E;
+}
+
+export interface ISearchableRepository<
+  E extends Entity,
+  EntityID extends ValueObject,
+  SearchInput = SearchParams,
+  SearchOutput = SearchResult,
+> extends IRepository<E, EntityID> {
+  sortableFields: string[];
+
+  search(props: SearchInput): Promise<SearchOutput>;
 }
