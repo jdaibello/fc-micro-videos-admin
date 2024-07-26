b1f6fb0fecf8d6fe13ee57786e2492089e23b00d
João Pedro <jpedrodaibello1999@outlook.com>
2024-07-26 15:47:47 -0300

    feat: create Nest.js project

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Specify the number of spaces per indentation-level.
-  tabWidth: 2,
-  singleQuote: true,
-  printWidth: 120,
-  trailingComma: "all",
-  semi: true,
-  bracketSpacing: true,
-};
