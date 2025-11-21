a6f2543cd882b5e7fcce6192e2d31e9836962e99
João Pedro <jpedrodaibello1999@outlook.com>
2025-11-21 14:53:14 -0300

    refactor: create database module

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -1,38 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
-import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
-import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
-import { SequelizeModule, getModelToken } from '@nestjs/sequelize';
+import { DatabaseModule } from 'src/database/database.module';
+import { CategoriesModule } from './categories.module';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        SequelizeModule.forRoot({
-          dialect: 'sqlite' as any,
-          host: ':memory:',
-          logging: false,
-          models: [CategoryModel],
-        }),
-        SequelizeModule.forFeature([CategoryModel]),
-      ],
-      controllers: [CategoriesController],
-      providers: [
-        {
-          provide: CategorySequelizeRepository,
-          useFactory: (categoryModel: typeof CategoryModel) => new CategorySequelizeRepository(categoryModel),
-          inject: [getModelToken(CategoryModel)],
-        },
-      ],
+      imports: [DatabaseModule, CategoriesModule],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
   });
 
   it('should be defined', () => {
-    console.log(controller);
     expect(controller).toBeDefined();
   });
 });
