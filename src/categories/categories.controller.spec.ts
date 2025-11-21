5f0fd4ebfcb211ea386cb2b3178f1e87918c3b80
João Pedro <jpedrodaibello1999@outlook.com>
2025-11-21 14:28:38 -0300

    feat: configure CategoriesController test

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -1,18 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
+import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
+import { SequelizeModule, getModelToken } from '@nestjs/sequelize';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        SequelizeModule.forRoot({
+          dialect: 'sqlite' as any,
+          host: ':memory:',
+          logging: false,
+          models: [CategoryModel],
+        }),
+        SequelizeModule.forFeature([CategoryModel]),
+      ],
       controllers: [CategoriesController],
+      providers: [
+        {
+          provide: CategorySequelizeRepository,
+          useFactory: (categoryModel: typeof CategoryModel) => new CategorySequelizeRepository(categoryModel),
+          inject: [getModelToken(CategoryModel)],
+        },
+      ],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
   });
 
   it('should be defined', () => {
+    console.log(controller);
     expect(controller).toBeDefined();
   });
 });
