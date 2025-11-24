c17effe23edb4cb54afb2d351099228464713a62
João Pedro <jpedrodaibello1999@outlook.com>
2025-11-24 14:33:51 -0300

    feat: implement get category by id endpoint

--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -37,7 +37,11 @@ export class CategoriesController {
   findAll() {}
 
   @Get(':id')
-  findOne(@Param('id') id: string) {}
+  async findOne(@Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string) {
+    const output = await this.getUseCase.execute({ id });
+
+    return CategoriesController.serialize(output);
+  }
 
   @Patch(':id')
   async update(
