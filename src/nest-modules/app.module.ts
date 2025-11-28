a22876c2ffa06e7e80a05bf93e1837ff17d181ff
João Pedro <jpedrodaibello1999@outlook.com>
2025-11-28 17:26:37 -0300

    refactor: move app module and main to src folder

--- src/nest-modules/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CategoriesModule } from './categories-module/categories.module';
-import { DatabaseModule } from './database-module/database.module';
-import { ConfigModule } from './config-module/config.module';
-
-@Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
-})
-export class AppModule {}
